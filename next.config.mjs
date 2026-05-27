b4c4900688ae0184c0899f4d57c55e5204741c56
yashvibing <277458935+yashvibing@users.noreply.github.com>
2026-05-27 11:34:35 +0530

    fix(qa): ISSUE-002 allow yield icon hosts

--- next.config.mjs
+++ next.config.mjs
@@ -17,6 +17,22 @@ const nextConfig = {
         protocol: "https",
         hostname: "icons.llamao.fi",
       },
+      {
+        protocol: "https",
+        hostname: "pbs.twimg.com",
+      },
+      {
+        protocol: "https",
+        hostname: "raw.githubusercontent.com",
+      },
+      {
+        protocol: "https",
+        hostname: "storage.googleapis.com",
+      },
+      {
+        protocol: "https",
+        hostname: "www.upshift.finance",
+      },
     ],
   },
 };
