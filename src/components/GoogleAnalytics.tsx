82b40611765c918c4349fae1dff75927876bedbe
yashvibing <yashwork2666@gmail.com>
2026-06-23 16:58:13 +0530

    Add Google Analytics tracking component

---
+++ src/components/GoogleAnalytics.tsx
@@ -0,0 +1,51 @@
+"use client";
+
+import Script from "next/script";
+import { usePathname } from "next/navigation";
+import { useEffect } from "react";
+
+declare global {
+  interface Window {
+    dataLayer?: unknown[];
+    gtag?: (...args: unknown[]) => void;
+  }
+}
+
+interface GoogleAnalyticsProps {
+  measurementId?: string;
+}
+
+export function GoogleAnalytics({ measurementId }: GoogleAnalyticsProps) {
+  const pathname = usePathname();
+
+  useEffect(() => {
+    if (!measurementId || !window.gtag) return;
+
+    window.gtag("config", measurementId, {
+      page_location: window.location.href,
+      page_path: `${window.location.pathname}${window.location.search}`,
+    });
+  }, [measurementId, pathname]);
+
+  if (!measurementId) return null;
+
+  const initScript = `
+    window.dataLayer = window.dataLayer || [];
+    function gtag(){window.dataLayer.push(arguments);}
+    window.gtag = gtag;
+    gtag('js', new Date());
+    gtag('config', ${JSON.stringify(measurementId)}, { send_page_view: false });
+  `;
+
+  return (
+    <>
+      <Script
+        src={`https://www.googletagmanager.com/gtag/js?id=${encodeURIComponent(measurementId)}`}
+        strategy="afterInteractive"
+      />
+      <Script id="google-analytics-init" strategy="afterInteractive">
+        {initScript}
+      </Script>
+    </>
+  );
+}
